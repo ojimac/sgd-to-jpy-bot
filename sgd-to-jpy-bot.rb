5110ad7a97259efeda21e601cc968d6033d6ea86
ojimac <ojimac@gmail.com>
2013-06-22 11:27:15 +0000

    実行モード切り替え、cronから実行できるようにした

--- sgd-to-jpy-bot.rb
+++ sgd-to-jpy-bot.rb
@@ -2,14 +2,26 @@
 
 require 'nokogiri'
 require 'open-uri'
-require 'twitter'
 require 'pp'
+require 'twitter'
+require 'yaml'
+
+case ARGV[0]
+when 'production'
+  CRON_ENV = 'production'
+else
+  CRON_ENV = 'development'
+end
+
+base_path   = File.expand_path(File.dirname(__FILE__));
+config_file = base_path + '/config.yml'
+config      = YAML.load_file(config_file)[CRON_ENV]
 
 Twitter.configure do |t|
-  t.consumer_key       = ''
-  t.consumer_secret    = ''
-  t.oauth_token        = ''
-  t.oauth_token_secret = ''
+  t.consumer_key       = config['consumer_key']
+  t.consumer_secret    = config['consumer_secret']
+  t.oauth_token        = config['oauth_token']
+  t.oauth_token_secret = config['oauth_token_secret']
 end
 
 doc = Nokogiri::HTML(open('http://info.finance.yahoo.co.jp/exchange/convert/?a=1&s=SGD&t=JPY'))
